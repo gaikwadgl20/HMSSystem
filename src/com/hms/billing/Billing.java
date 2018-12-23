16b16efce376a08484a48c0f3e6fc52101726e73
Ganesh Gaikwad <gaikwadgl20@gmail.com>
2018-12-23 22:37:47 +0530

    code formating

--- src/com/hms/billing/Billing.java
+++ src/com/hms/billing/Billing.java
@@ -1,62 +1,54 @@
 package com.hms.billing;
 
-import com.hms.users.*;
+import com.hms.users.Patient;
 
 public class Billing {
-	   
-    public static double[] computePaymentAmount(Patient patient, double amount) {
-        double[] payments = new double[2];
-        
-        HealthInsurancePlan patientInsurancePlan = patient.getInsurancePlan();
-        
-        if(patientInsurancePlan == null) {
-        	payments[0]=0;
-        	payments[1]= amount - 20; 
-        }
-        else if(patientInsurancePlan instanceof GoldPlan) {
-        	((GoldPlan) patientInsurancePlan).goldCoverage();
-        	payments[0] = amount * patientInsurancePlan.getCoverage();
-        	payments[1] = amount - payments[0] - 40;
-        }
-        else if(patientInsurancePlan instanceof PlatinumPlan) {
-        	((PlatinumPlan) patientInsurancePlan).plataniumCoverage();
-        	payments[0] = amount * patientInsurancePlan.getCoverage();
-        	payments[1] = amount - payments[0] - 50;
-        }
-        else if(patientInsurancePlan instanceof SilverPlan) {
-        	((SilverPlan) patientInsurancePlan).silverCoverage();
-        	payments[0] = amount * patientInsurancePlan.getCoverage();
-        	payments[1] = amount - payments[0] - 30;
-        }
-        else {
-        	((BronzePlan) patientInsurancePlan).bronzeCoverage();
-        	payments[0] = amount * patientInsurancePlan.getCoverage();
-        	payments[1] = amount - payments[0] - 25;
-        }
-        // your logic        
 
-        return payments;
-    }
-  /*  
-    public static double monthlyPremium(User user, double amount) {
-    	HealthInsurancePlan userPlan = user.getInsurancePlan();
-    	double monthPremiun =0 ;
-    	if(user.getInsurancePlan() == null) {
-    		monthPremiun = 0;
-    	}
-    	else if(userPlan instanceof GoldPlan) {
-    		monthPremiun = userPlan.computeMonthlyPremium(amount);
-        }
-    	else if(userPlan instanceof PlatinumPlan) {
-    		monthPremiun = userPlan.computeMonthlyPremium(amount);
-        }
-    	else if(userPlan instanceof SilverPlan) {
-    		monthPremiun = userPlan.computeMonthlyPremium(amount);
-        }
-    	else if(userPlan instanceof BronzePlan) {
-    		monthPremiun = userPlan.computeMonthlyPremium(amount);
-        }    	
-    	return monthPremiun;
-    }*/
-    
+	public static double[] computePaymentAmount(Patient patient, double amount) {
+		double[] payments = new double[2];
+
+		HealthInsurancePlan patientInsurancePlan = patient.getInsurancePlan();
+
+		if (patientInsurancePlan == null) {
+			payments[0] = 0;
+			payments[1] = amount - 20;
+		} else if (patientInsurancePlan instanceof GoldPlan) {
+			((GoldPlan) patientInsurancePlan).goldCoverage();
+			payments[0] = amount * patientInsurancePlan.getCoverage();
+			payments[1] = amount - payments[0] - 40;
+		} else if (patientInsurancePlan instanceof PlatinumPlan) {
+			((PlatinumPlan) patientInsurancePlan).plataniumCoverage();
+			payments[0] = amount * patientInsurancePlan.getCoverage();
+			payments[1] = amount - payments[0] - 50;
+		} else if (patientInsurancePlan instanceof SilverPlan) {
+			((SilverPlan) patientInsurancePlan).silverCoverage();
+			payments[0] = amount * patientInsurancePlan.getCoverage();
+			payments[1] = amount - payments[0] - 30;
+		} else {
+			((BronzePlan) patientInsurancePlan).bronzeCoverage();
+			payments[0] = amount * patientInsurancePlan.getCoverage();
+			payments[1] = amount - payments[0] - 25;
+		}
+		// your logic
+
+		return payments;
+	}
+
+	/*public static double monthlyPremium(User user, double amount) {
+		HealthInsurancePlan userPlan = user.getInsurancePlan();
+		double monthPremiun = 0;
+		if (user.getInsurancePlan() == null) {
+			monthPremiun = 0;
+		} else if (userPlan instanceof GoldPlan) {
+			monthPremiun = userPlan.computeMonthlyPremium(amount);
+		} else if (userPlan instanceof PlatinumPlan) {
+			monthPremiun = userPlan.computeMonthlyPremium(amount);
+		} else if (userPlan instanceof SilverPlan) {
+			monthPremiun = userPlan.computeMonthlyPremium(amount);
+		} else if (userPlan instanceof BronzePlan) {
+			monthPremiun = userPlan.computeMonthlyPremium(amount);
+		}
+		return monthPremiun;
+	}*/
+
 }
--- src/com/hms/billing/TestBilling.java
+++ src/com/hms/billing/TestBilling.java
@@ -1,36 +1,37 @@
 package com.hms.billing;
 
-
-import com.hms.users.*;
-
+import com.hms.users.Nurse;
+import com.hms.users.Patient;
+import com.hms.users.User;
 
 public class TestBilling {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		HealthInsurancePlan insurancePlan = new PlatinumPlan();        
+		HealthInsurancePlan insurancePlan = new PlatinumPlan();
 		Patient patient = new Patient();
 		patient.setInsurancePlan(insurancePlan);
-		
+
 		Nurse nurse = new Nurse();
 		nurse.setInsurancePlan(insurancePlan);
-		
-		User staff = new User();                        
-		InsuranceBrand insuranceBrand = new BlueCrossBlueShield();           
-		HealthInsurancePlan insurancePlan1 = new PlatinumPlan(); 
-		 
-		
+
+		User staff = new User();
+		InsuranceBrand insuranceBrand = new BlueCrossBlueShield();
+		HealthInsurancePlan insurancePlan1 = new PlatinumPlan();
+
 		insurancePlan1.setOfferedBy(insuranceBrand);
 		staff.setInsurancePlan(insurancePlan1);
-		System.out.println("Monthly Premium : "+insurancePlan1.computeMonthlyPremium(5000, 56, true));
-		
-		/*double monthlyPremium = Billing.monthlyPremium(nurse, 5000);
-		System.out.println(" Montly premium fot staff:" + monthlyPremium);*/
-		        
+		System.out.println("Monthly Premium : " + insurancePlan1.computeMonthlyPremium(5000, 56, true));
+
+		/*
+		 * double monthlyPremium = Billing.monthlyPremium(nurse, 5000);
+		 * System.out.println(" Montly premium fot staff:" + monthlyPremium);
+		 */
+
 		double[] payments = Billing.computePaymentAmount(patient, 1000.0);
-		for(double bill :payments) {
+		for (double bill : payments) {
 			System.out.println(bill);
-		}		
+		}
 	}
 
 }
